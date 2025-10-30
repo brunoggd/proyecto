3aae34f84ec1ebe848524e65386479bc31ed6cad
Bruno González <brunobrunito048@gmail.com>
2025-10-30 19:47:16 -0300

    Add files via upload

---
+++ proyecto_programacion/texto_informativo.py
@@ -0,0 +1,12 @@
+guia_informativa = """                                             Acerca de TDL
+
+• TDL es un gestor de tareas fácil de usar para organizar de una mejor manera tu día a día, cuenta con las siguientes herramientas para hacer la gestión de tu vida más sencilla, eficaz y llevadera:
+
+    • Calendario para gestionar cuando tenés que hacer eso que llevas organizando tanto tiempo.
+
+    • Validación de campos específicos: Distribuye tareas de forma específica con campos para determinar los aspectos más importantes de las mismas.
+
+    • Creación, Modificación y Eliminación, los pilares fundamentales de una buena organización. Cuando termines alguna tarea, puedes borrarla, cambiar algo ya sea que te surgió un percance o algo similar, o seguir agregando cosas que tengas para hacer.
+    
+    • Personalización de la interfaz: Si eres alguien cuya vista se canza demasiado rápido ante un diseño claro y con colores contrastantes, entonces la opción de modo oscuro es tu salvación. Sin olvidar mencionar otras opciones para personalizar aún más tu experiencia.
+    """
